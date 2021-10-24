c087b1f5f6375d34f0406d4a4b5431d3b2b545ec
HarishMudimannan <mannanharish@gmail.com>
2021-10-24 16:48:21 +0530

    SafariBrowser

---
+++ Web-Automation/src/com/sgtesting/selenium/SafariBrowser.java
@@ -0,0 +1,61 @@
+package com.sgtesting.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class SafariBrowserDemo{
+	public static WebDriver wb;
+	static void LaunchBrowser() {
+		try {
+			System.setProperty("webdriver.chrome.driver", "D:\\Java\\Web Driver\\chromedriver_win32\\chromedriver.exe");
+			wb = new ChromeDriver();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	static void navigate() {
+		try {
+			wb.get("http://localhost:82/login.do");
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	static void closeapplication() {
+		try {
+			wb.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static void login(){
+		try {
+			wb.findElement(By.id("username")).sendKeys("admin");
+			wb.findElement(By.name("pwd")).sendKeys("manager");
+			wb.findElement(By.xpath("//*[@id=\'loginButton\']/div")).click();
+			Thread.sleep(4000);
+			wb.findElement(By.xpath("//*[@id=\'gettingStartedShortcutsPanelId\']")).click();
+			Thread.sleep(5000);
+			wb.findElement(By.linkText("Logout")).click();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
+public class SafariBrowser {
+
+	public static void main(String[] args) {
+		SafariBrowserDemo.LaunchBrowser();
+		SafariBrowserDemo.navigate();
+		SafariBrowserDemo.login();
+		SafariBrowserDemo.closeapplication();
+	}
+
+}
